4119f1495be3228265254b7e12f60a19201026be
Alfrheim <marc.badiac@gmail.com>
2017-06-06 22:22:07 +0200

    fixed floobits deletion

--- src/main/java/io/github/alfrheim/controllers/ReservationController.java
+++ src/main/java/io/github/alfrheim/controllers/ReservationController.java
@@ -1,10 +1,20 @@
 package io.github.alfrheim.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.PATCH;
 
 /**
  * @author alfrheim on 06/06/17.
  */
-@RestController
+@RestController("/stock/v2/reservation/{reservation_id}")
 public class ReservationController {
+
+    @RequestMapping(method = PATCH)
+    public ReponseEntity<Reservation> addItemReservation( @PathVariable("reservation_id") String reservationId) {
+
+    }
 }
